a1671aa78f5e4da1cc974cd48dbb066717439bb2
niki32 <52590931+niki32@users.noreply.github.com>
2019-07-13 22:37:06 -0700

    Update course manager.py

--- course manager.py
+++ course manager.py
@@ -9,21 +9,41 @@ class Course:
 
 
     def addCourse(self):
+        list = []
+        list2 = []
+        list3 = []
         self.name = str(input("Enter the course name: "))
         self.unit = int(input("Enter the number of units"))
         self.term = input("Enter the term you are taking this course: ")
+        list.append(self.name)
+        list2.append(self.unit)
+        list3.append(self.term)
 
-
-    #def search (self):
-
-    #def removecourse(self):
-
+    def removeCourse(self):
+        remove = input("Enter the course name to drop: ")
+        if remove in list:
+            list.pop(self.name)
+        else:
+            print(self.name, " is not found in your course list.")
 
 
     def printlist (self):
+        print("Course", end="         ")
+        print("Unit", end="        ")
+        print("Term")
         print(self.name)
         print(self.unit)
         print(self.term)
+
+    def insertionSort(lst):
+        for i in range(1, len(lst)):
+            # insert lst[i] into a sorted sublist lst[0..i-1] so that
+            #   lst[0..i] is sorted.
+            currentElement = lst[i]
+            k = i - 1
+            while k >= 0 and lst[k] > currentElement:
+                lst[k + 1] = lst[k]
+                k -= 1
 
 
 def menu():
@@ -40,7 +60,8 @@ def menu():
 #program starts running
 loop = 1
 while loop == 1:
-    choice = menu()
+    choice = 1
+    menu()
     if choice == 1:
         Course.printlist()
     elif choice == 2:
