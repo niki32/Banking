7b2c01400050b31f54f85b9d163f6f222d6541a8
niki32 <52590931+niki32@users.noreply.github.com>
2019-07-15 14:22:24 -0700

    Update course manager.py

--- course manager.py
+++ course manager.py
@@ -1,3 +1,4 @@
+course_list = []
 class Course:
 
     def __init__(self, name, unit, term):
@@ -5,39 +6,43 @@ class Course:
         self.unit = unit
         self.term = term
 
+    def __lt__(self, other):
+        return self.unit < other.unit
 
     def addCourse(self):
-        course_list = []
-        self.name = input("enter class name: ")
-        self.unit = int(input("Enter class units: "))
-        self.term = int(input("Enter the term for this class"))
-        course1 = Course(self.name, self.unit, self.term)
+            self.name = str(input("Enter a new number: "))
+            self.unit = int(input("Enter class units: "))
+            self.term = (input("Enter the term for this class"))
+            course_list.append(self.name, self.unit, self.term)
+
+    def __lt__(self, other):
+        return self.unit < other.unit
 
     def removeCourse(self):
-        course_list = course1
-        remove = input("Enter the course name to drop: ")
-        if remove in course_list:
-            course_list.pop(self.name, self.unit, self.term)
+        remove = input("Enter that course that you would like to remove: ")
+        index = search(remove, course_list)
+        if index > -1:
+            print("found, index = ", index)
+            # change(course_list[index], "New name")
         else:
-            print(self.name, " is not found in your course list.")
+            print("Not found")
+
+    def printlist(self):
+        print("Course", end="     ")
+        print("Unit", end="     ")
+        print("Term")
+        return self.name + str(self.unit) + str(self.term)
 
 
-    def printlist (self):
-        if len(course_list) == 0:
-            print("No classes inputted.")
-        else:
-            print("no")
-
-
-    def insertionSort(lst):
-        for i in range(1, len(lst)):
+def insertionSort(lst):
+    for i in range(1, len(lst)):
             # insert lst[i] into a sorted sublist lst[0..i-1] so that
             #   lst[0..i] is sorted.
-            currentElement = lst[i]
-            k = i - 1
-            while k >= 0 and lst[k] > currentElement:
-                lst[k + 1] = lst[k]
-                k -= 1
+        currentElement = lst[i]
+        k = i - 1
+        while k >= 0 and lst[k] > currentElement:
+            lst[k + 1] = lst[k]
+            k -= 1
 
             # Insert the current element into lst[k + 1]
             lst[k + 1] = currentElement
@@ -52,14 +57,32 @@ def menu():
     print("  5. Sort courses based on number of units")
     print("  6. Exit")
     print(" ")
-    choice = input("Enter your option: ")
+    return eval(input("Enter your option: "))
 
 #program starts running
 loop = 1
+
+x = Course("", 0)
+
 while loop == 1:
-    choice = 1
-    menu()
+    choice = menu()
     if choice == 1:
-        Course.printlist()
+        if len(course_list) == 0:
+            print("No classes have been inputted.")
+        else:
+            print("no")
     elif choice == 2:
-        Course.addCourse()
+        x.addCourse()
+    elif choice ==3:
+        print("not ready yet")
+    elif choice == 4:
+        print("not ready yet")
+    elif choice == 5:
+        print("not ready yet")
+    elif choice == 6:
+        loop = 0
+        print("Thank you for using the my bank application.")  # end the application
+    elif choice <= 0:
+        print("Invalid selection. Please try again.")  # error checking
+    elif choice > 6:
+        print("Invalid selection. Please try again.")  # error checking
