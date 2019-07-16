ccd9f30f7dd59f424f283367feff65551d38ede1
niki32 <52590931+niki32@users.noreply.github.com>
2019-07-15 23:05:53 -0700

    Update course manager.py

--- course manager.py
+++ course manager.py
@@ -6,15 +6,15 @@ class Course:
         self.term = term
         self.unit = unit
 
+    def __str__(self):
+        return self.term, self.unit, self.name
 
     def __lt__(self, other):
         return self.unit < other.unit
 
-    def search(course_name, lst):
-        for i in range(0, len(lst)):
-            if course_name == lst[i].name:
-                return i
-        return -1
+    '''
+    This function allows the user to add a course to their course list.
+    '''
 
     def addCourse(self):
         self.name = str(input("Enter course name: "))
@@ -24,6 +24,10 @@ class Course:
         self.term = (input("Enter the term for this class: "))
         course_list.append(self.term)
 
+    '''
+    This function allows the user to drop a course from their course list.
+    '''
+
     def removeCourse(self):
         removed = input("Enter that course that you would like to remove: ")
         if removed in course_list:
@@ -31,25 +35,47 @@ class Course:
             course_list.remove(self.unit)
             course_list.remove(self.term)
         else:
-            print("This course does not exist.")
+            print("The course", removed, "does not exist.")
 
+    '''
+    This function allows the user to print their list.
+    '''
 
     def printList(self):
-        print("Course", end="     ")
-        print("Unit", end="     ")
+        print("Course", end="         ")
+        print("Unit", end="         ")
         print("Term")
         for x in range(len(course_list)):
-            print(course_list[x], end = "      ")
-
-        #return str(self.name) + str(self.unit) + str(self.term)
+            print(course_list[x],end="         ")
+    '''
+    This function allows the user to print their list in alphabetical order.
+    '''
 
     def courseSort(self):
-        course_list.sort()
-        print("Course", end="     ")
-        print("Unit", end="     ")
+        name_list = []
+        for x in course_list[::3]:
+            name_list.append(x)
+        name_list.sort()
+        print("Course", end="         ")
+        print("Unit", end="         ")
         print("Term")
-        for x in range(len(course_list)):
-            print(course_list[x], end="      ")
+        for n in range(len(name_list)):
+            print(name_list[n], end="       ")
+
+    def courseSortnum(self):
+        unit_list = []
+        for x in course_list[::1]:
+            unit_list.append(x)
+        unit_list.sort()
+        print("Course", end="         ")
+        print("Unit", end="         ")
+        print("Term")
+        for n in range(len(unit_list)):
+            print(unit_list[n], end="       ")
+            
+'''
+This function displays the menu.
+'''
 
 
 def menu():
@@ -71,22 +97,23 @@ Course1 = Course("", "", 0)
 
 while loop == 1:
     choice = menu()
-    if choice == 1:
+    if choice == 1: #option 1 is displayed
         if len(course_list) == 0:
             print("No classes have been inputted.")
         else:
             Course1.printList()
-    elif choice == 2:
+    elif choice == 2: #option 2 is displayed
         Course1.addCourse()
-    elif choice == 3:
+    elif choice == 3: #option 3 is displayed
         Course1.removeCourse()
-    elif choice == 4:
+    elif choice == 4: #option 4 is displayed
+        print("\n")
         Course1.courseSort()
-    elif choice == 5:
-        print("not ready yet")
-    elif choice == 6:
+    elif choice == 5: #option 5 is displayed
+        Course1.courseSortnum()
+    elif choice == 6: #option 6 is displayed
         loop = 0
-        print("Thank you for using the my bank application.")  # end the application
+        print("Thank you for using the course list application")  # end the application
     elif choice <= 0:
         print("Invalid selection. Please try again.")  # error checking
     elif choice > 6:
